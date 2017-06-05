194f065ee2b525fe5d7bdea9b9e6706a275c137a
Davo Faulkner <d@davo.co>
2017-06-04 20:22:46 -0500

    Add Article class with basic fields

---
+++ app/src/main/java/co/davo/news/Article.java
@@ -0,0 +1,37 @@
+package co.davo.news;
+
+import java.util.Date;
+
+/**
+ * Created by Davo on 6/4/2017.
+ */
+
+public class Article {
+    private String title;
+    private String section;
+    private String author;
+    private Date publicationDate;
+
+    public Article(String title, String section, String author, Date publicationDate) {
+        this.title = title;
+        this.section = section;
+        this.author = author;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+}
