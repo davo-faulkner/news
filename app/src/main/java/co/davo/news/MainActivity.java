ad6e69efb8001bfe8f3e296f10e961b38b451be1
Davo Faulkner <d@davo.co>
2017-06-04 23:36:04 -0500

    Add @Override methods for LoaderManager to MainActivity.java

--- app/src/main/java/co/davo/news/MainActivity.java
+++ app/src/main/java/co/davo/news/MainActivity.java
@@ -1,13 +1,32 @@
 package co.davo.news;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Article>> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public Loader<ArrayList<Article>> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<Article>> loader, ArrayList<Article> data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<Article>> loader) {
+
     }
 }
