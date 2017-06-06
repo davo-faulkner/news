bcadd71923bfb21edafc9fe2a762219638f97452
Davo Faulkner <d@davo.co>
2017-06-05 23:05:23 -0500

    Add ArticleHolder inner class to RecyclerAdapter.java with constructor and onClick()

--- app/src/main/java/co/davo/news/RecyclerAdapter.java
+++ app/src/main/java/co/davo/news/RecyclerAdapter.java
@@ -1,6 +1,7 @@
 package co.davo.news;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
@@ -14,6 +15,18 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Articl
 
     public RecyclerAdapter(ArrayList<Article> articles) {
         this.articles = articles;
+    }
+
+    public static class ArticleHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ArticleHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 
     @Override
