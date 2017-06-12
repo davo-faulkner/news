ce26b87dfc43e5f66429a9c06c4642ee97bcca67
Davo Faulkner <d@davo.co>
2017-06-11 20:51:04 -0500

    Continue coding QueryUtils.java

--- app/src/main/java/co/davo/news/QueryUtils.java
+++ app/src/main/java/co/davo/news/QueryUtils.java
@@ -2,10 +2,6 @@ package co.davo.news;
 
 import android.util.Log;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,10 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 import static co.davo.news.MainActivity.LOG_TAG;
 
@@ -47,8 +40,63 @@ public final class QueryUtils {
     private static String fetchArticleData(String requestUrl) {
         URL url = createUrl(requestUrl);
         String articlesJsonString = null;
-
+        try {
+            articlesJsonString = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error closing input stream", e);
+            MainActivity.setHasIoException(true);
+        }
         return articlesJsonString;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        if (url == null) {
+            return jsonResponse;
+        }
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                String badResponseCode = "Error response code: " + urlConnection.getResponseCode();
+                Log.e(LOG_TAG, badResponseCode);
+                MainActivity.setHasBadResponseCode(true);
+                MainActivity.setBadResponseCode(urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the article JSON results", e);
+            MainActivity.setHasIoException2(true);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
     }
 
     private static URL createUrl(String stringUrl) {
@@ -62,5 +110,4 @@ public final class QueryUtils {
         }
         return url;
     }
-
 }
